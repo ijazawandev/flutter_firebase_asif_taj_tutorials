b93044db02648bc8f13d4529805ec4210ad9cd7c
ijazawandev <ij.ah8267@gmail.com>
2024-03-11 07:18:20 +0500

    added icon logout on post screen

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,16 +1,34 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_asif_taj_tutorials/post/post_screen.dart';
 import 'package:flutter_firebase_asif_taj_tutorials/ui/auth/login_screen.dart';
 
 class SplashServices {
   void isLogin(BuildContext context) {
-    Timer.periodic(const Duration(seconds: 3), (timer) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => LoginScreen(),
-        ),
-      );
-    });
+    final auth =FirebaseAuth.instance;
+    final user=auth.currentUser;
+    if(user !=null){
+
+      Timer.periodic(const Duration(seconds: 3), (timer) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PostScreen(),
+          ),
+        );
+      });
+
+    }else{
+
+      Timer.periodic(const Duration(seconds: 3), (timer) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LoginScreen(),
+          ),
+        );
+      });
+    }
   }
 }
