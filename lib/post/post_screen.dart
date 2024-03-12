0653b8c866971759c0901f97c027eab1ea4da46d
ijazawandev <ij.ah8267@gmail.com>
2024-03-12 08:28:40 +0500

    added icon logout on post screen add postscreen and verify with 6 digits code verification screen

--- lib/post/post_screen.dart
+++ lib/post/post_screen.dart
@@ -1,4 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_asif_taj_tutorials/post/add_posts.dart';
 import 'package:flutter_firebase_asif_taj_tutorials/ui/auth/login_screen.dart';
@@ -13,6 +17,8 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   final auth=FirebaseAuth.instance;
+  final ref=FirebaseDatabase.instance.ref('Post');
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +35,24 @@ class _PostScreenState extends State<PostScreen> {
        }, icon: Icon(Icons.logout_outlined),),
        SizedBox(width: 10,),
      ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FirebaseAnimatedList
+              (query: ref,
+                defaultChild: Text('Loading'),
+                itemBuilder:(context,snapshot,animation,index){{
+                  return  ListTile(
+                    title: Text(snapshot.child('title').value.toString()),
+                  );
+                }
+                } ),
+          ),
+          // ListTile(
+          //   title: Text('Hello'),
+          // ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(onPressed: (){
 Navigator.push(context, MaterialPageRoute(builder: (context)=>AddPostScreen()));
