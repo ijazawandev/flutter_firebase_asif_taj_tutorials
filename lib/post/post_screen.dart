28ef9d28b3829f6abc48c666d1d39f748e1649c3
ijazawandev <ij.ah8267@gmail.com>
2024-03-12 10:52:56 +0500

    show data on screen with real time database of firebase

--- lib/post/post_screen.dart
+++ lib/post/post_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:ffi';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_firebase_asif_taj_tutorials/post/add_posts.dart';
 import 'package:flutter_firebase_asif_taj_tutorials/ui/auth/login_screen.dart';
 import 'package:flutter_firebase_asif_taj_tutorials/util/utils.dart';
@@ -12,53 +15,102 @@ class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
 
   @override
-    State<PostScreen> createState() => _PostScreenState();
+  State<PostScreen> createState() => _PostScreenState();
 }
 
 class _PostScreenState extends State<PostScreen> {
-  final auth=FirebaseAuth.instance;
-  final ref=FirebaseDatabase.instance.ref('Post');
+  final auth = FirebaseAuth.instance;
+  final ref = FirebaseDatabase.instance.ref('Post');
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    ref.onValue.listen((event) {
+
+
+    });
+  }
+
   
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Center(child: Text('Post')),
-     actions: [
-       IconButton(onPressed: (){
-         auth.signOut().then((value) {
-           Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
-         }).onError((error, stackTrace) {
-           Utils().toastMessage(error.toString());
-         });
-       }, icon: Icon(Icons.logout_outlined),),
-       SizedBox(width: 10,),
-     ],
+        actions: [
+          IconButton(
+            onPressed: () {
+              auth.signOut().then((value) {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              }).onError((error, stackTrace) {
+                Utils().toastMessage(error.toString());
+              });
+            },
+            icon: Icon(Icons.logout_outlined),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+        ],
       ),
       body: Column(
         children: [
+          // Expanded(
+          //   child: StreamBuilder(
+          //               stream: ref.onValue,
+          //               builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+          //   Map<dynamic,dynamic> map=snapshot.data!.snapshot.value as dynamic;
+          //   List<dynamic>list=[];
+          //   list.clear();
+          //   list=map.values.toList();
+          //   if(!snapshot.hasData){
+          //     return CircularProgressIndicator();
+          //
+          //   }else{
+          //
+          //   }
+          //   return ListView.builder(
+          //       itemCount: snapshot.data?.snapshot.children.length,
+          //       itemBuilder: (context, index) {
+          //     return ListTile(
+          //       title: Text(list[index]['title']),
+          //        subtitle: Text(list[index]['id']),
+          //     );
+          //   });
+          //               }, // stream: null, // stream error//
+          //             ),
+          // ),
           Expanded(
-            child: FirebaseAnimatedList
-              (query: ref,
+            child: FirebaseAnimatedList(
+                query: ref,
                 defaultChild: Text('Loading'),
-                itemBuilder:(context,snapshot,animation,index){{
-                  return  ListTile(
-                    title: Text(snapshot.child('title').value.toString()),
-                  );
-                }
-                } ),
+                itemBuilder: (context, snapshot, animation, index) {
+                  {
+                    var list;
+                    return ListTile(
+                     // title: Text(list[index]['title']),
+                        
+                    //  subtitle: Text(list[index]['id']),
+                       title: Text(snapshot.child('title').value.toString()),
+                      subtitle: Text(snapshot.child('id').value.toString()),
+                    );
+                  }
+                }),
           ),
           // ListTile(
           //   title: Text('Hello'),
           // ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-Navigator.push(context, MaterialPageRoute(builder: (context)=>AddPostScreen()));
-
-      },
-      child: Icon(Icons.add),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AddPostScreen()));
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
